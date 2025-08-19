b20f88fd2e6cebdb455df89a996a8081ad48d4a5
Vinayak180302 <vinayakkumkar2002@gmail.com>
2025-08-19 12:30:15 +0530

    code for sort array

---
+++ IsSorted.java
@@ -0,0 +1,18 @@
+public class IsSorted {
+
+  public static boolean isSorted(int arr[]){
+    for(int i=1; i<arr.length; i++){
+      if(arr[i] >= arr[i-1]){
+
+      }
+      else{
+        return false;
+      }
+    }
+    return true;
+  }
+  public static void main(String[] args) {
+    int arr[] = {1, 2, 2, 4, 5, 8, 9};
+    System.out.println(isSorted(arr));
+  }
+}
